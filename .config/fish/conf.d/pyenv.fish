e7c6ae747e2ae66689cb27bc0456b82ff238a702
Pat Ambrosio <1647347+hmngwy@users.noreply.github.com>
2021-05-29 00:40:58 +0800

    Update pyenv.fish

--- .config/fish/conf.d/pyenv.fish
+++ .config/fish/conf.d/pyenv.fish
@@ -1,7 +1,8 @@
-set -q PYENV_ROOT; or set -l PYENV_ROOT $HOME/.pyenv
+if type -q pyenv
+  set -q PYENV_ROOT; or set -l PYENV_ROOT $HOME/.pyenv
+  set PATH $PYENV_ROOT/libexec/pyenv $PYENV_ROOT/libexec $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH 2> /dev/null
+  setenv PYENV_SHELL fish
 
-set PATH $PYENV_ROOT/libexec/pyenv $PYENV_ROOT/libexec $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH 2> /dev/null
-setenv PYENV_SHELL fish
-
-status --is-interactive; and pyenv init - | source
-status --is-interactive; and pyenv virtualenv-init - | source
+  status --is-interactive; and pyenv init - | source
+  status --is-interactive; and pyenv virtualenv-init - | source
+end
