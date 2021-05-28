8a88acbd7421c7fa28d6d83c6d1ffe7a740b46ba
Pat Ambrosio <1647347+hmngwy@users.noreply.github.com>
2021-05-29 03:51:24 +0800

    Update fish.sh

--- .config/setup/fish.sh
+++ .config/setup/fish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
-sudo apt install fish
+sudo apt install -y fish
 fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher'
 fish -c 'fisher update'
