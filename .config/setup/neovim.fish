73fe46e684b81695579cbb27358d0aec48e6581f
Pat Ambrosio <1647347+hmngwy@users.noreply.github.com>
2021-05-31 15:49:46 +0800

    Update neovim.fish

--- .config/setup/neovim.fish
+++ .config/setup/neovim.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/fish
 sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt install neovim
+sudo apt install -y neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
