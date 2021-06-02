204c51c404dcf9bc45b82390b3dbee28f7b581c7
Pat Ambrosio <1647347+hmngwy@users.noreply.github.com>
2021-06-02 18:23:06 +0800

    Update submodules.sh

--- .config/setup/submodules.sh
+++ .config/setup/submodules.sh
@@ -2,6 +2,6 @@
 dot submodule update --init --recursive
 
 # setup submodules
-cd $HOME/.asdf/; git checkout "$(git describe --abbrev=0 --tags)"
+cd $HOME/.asdf/; git checkout "(git describe --abbrev=0 --tags)"
 mkdir -p $HOME/.config/fish/completions; ln -s $HOME/.asdf/completions/asdf.fish $HOME/.config/fish/completions
 $HOME/.fzf/install --all
