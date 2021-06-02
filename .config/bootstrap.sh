4976df4d8aaedacf524e90fc5e9a141ae52a65bd
Pat Ambrosio <1647347+hmngwy@users.noreply.github.com>
2021-06-02 13:21:42 +0800

    Update bootstrap.sh

--- .config/bootstrap.sh
+++ .config/bootstrap.sh
@@ -21,6 +21,7 @@ config submodule update --init --recursive
 # setup submodules
 cd ~/.asdf/; git checkout "$(git describe --abbrev=0 --tags)"
 mkdir -p ~/.config/fish/completions; ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+~/.fzf/install --all
 
-
-cd ~/.fzf/; ./install --all
+# install docker
+curl -fsSL https://get.docker.com | sh
