a057d2b57845865a90e97c4e7e15205232d4c9d4
hmngwy <1647347+hmngwy@users.noreply.github.com>
2021-06-04 00:05:42 +0800

    changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
 if test -e ~/.asdf/asdf.fish
     source ~/.asdf/asdf.fish
 end
+if set -q VIRTUAL_ENV
+    echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+end
