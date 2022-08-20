42af0296a62cadcf2cbbe0d5f5f35c2b20402177
hmngwy <1647347+hmngwy@users.noreply.github.com>
2022-08-20 13:01:39 +0800

    changes

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,3 +1,4 @@
 alias dot='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias n='nvim'
+alias x='xplr'
 alias arst='asdf'
---
+++ .config/fish/conf.d/paths.fish
@@ -0,0 +1,3 @@
+fish_add_path $HOME/.amplify/bin/
+fish_add_path $HOME/.cargo/bin/
+fish_add_path $HOME/.local/bin/
