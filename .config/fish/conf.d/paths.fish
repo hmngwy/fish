5bbef3f052cb60008ef4f87c48515a7b39f157fd
hmngwy <1647347+hmngwy@users.noreply.github.com>
2022-08-30 00:43:00 +0800

    changes

--- .config/fish/conf.d/paths.fish
+++ .config/fish/conf.d/paths.fish
@@ -1,3 +1,4 @@
 fish_add_path $HOME/.amplify/bin/
 fish_add_path $HOME/.cargo/bin/
 fish_add_path $HOME/.local/bin/
+fish_add_path $HOME/.outpost/gui/wezterm/target/release/
---
+++ .config/fish/conf.d/xrdb.fish
@@ -0,0 +1,1 @@
+xrdb $HOME/.Xresources
